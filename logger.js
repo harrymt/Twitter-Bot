f908497d5f09f7d16fd4787d1d8365247055c714
Harry Mumford-Turner <harrymt@users.noreply.github.com>
2015-06-30 12:27:14 +0100

    Testing pushing the logger to browser

--- logger.js
+++ logger.js
@@ -1,5 +1,14 @@
 
+
+
 exports.write = function(message) {
+  var http = require('http');
+
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(message + '\n');
+  }).listen();
+
   var d = new Date().toISOString();
   console.log(d.replace("T"," ").substring(0, d.length - 5) + " | " + message);
 };
