508ab46ba4888cff60ce30a5caf34ffcc24a78cd
Harry Mumford-Turner <harrymt@users.noreply.github.com>
2015-06-30 10:10:36 +0100

    Updated the test descriptions

--- test/test.js
+++ test/test.js
@@ -5,24 +5,14 @@ describe('Admin', function() {
   
   // Initial
   describe('Testing the tests', function() {
-    it('Initial basic test that should always pass', function() {
+    it('should always pass, to test these tests', function() {
       assert.equal(-1, -1);
     })
   })
   
-  
   describe('Environment variables', function() {
-    it('Test the environment variables', function() {
+    it('should match the test environment variable we setup', function() {
       assert.equal(process.env.test_env, "aaa");
-      
-//       // Get the values from Travis CI encrypted environment variables
-// var keys = {
-//   "consumer_secret": process.env.keys_consumer_secret,
-//   "consumer_key": process.env.keys_consumer_key,
-//   "access_token": process.env.keys_access_token,
-//   "access_token_secret": process.env.keys_access_token_secret
-// };
-
     })
   })
   
