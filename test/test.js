560230211437cdaa22df36c401a82573733cf1db
Harry Mumford-Turner <harrymt@users.noreply.github.com>
2015-06-30 10:08:04 +0100

    Added environment variable test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,29 @@
 
 var assert = require("assert")
 
-describe('Category A', function() {
+describe('Admin', function() {
+  
+  // Initial
   describe('Testing the tests', function() {
     it('Initial basic test that should always pass', function() {
       assert.equal(-1, -1);
     })
   })
+  
+  
+  describe('Environment variables', function() {
+    it('Test the environment variables', function() {
+      assert.equal(process.env.test_env, "aaa");
+      
+//       // Get the values from Travis CI encrypted environment variables
+// var keys = {
+//   "consumer_secret": process.env.keys_consumer_secret,
+//   "consumer_key": process.env.keys_consumer_key,
+//   "access_token": process.env.keys_access_token,
+//   "access_token_secret": process.env.keys_access_token_secret
+// };
+
+    })
+  })
+  
 })
